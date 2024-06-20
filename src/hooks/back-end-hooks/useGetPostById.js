b18d1fa599078b47fb78b313ace0a050b4f5095f
sinox2003 <mohaennouass@gmail.com>
2024-06-20 21:10:34 +0100

    update

--- src/hooks/back-end-hooks/useGetPostById.js
+++ src/hooks/back-end-hooks/useGetPostById.js
@@ -1,35 +1,31 @@
 import { useEffect, useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import useShowToast from "../useShowToast.jsx";
-import {firestore} from "../../config/firebase.js";
-
+import { firestore } from "../../config/firebase.js";
 
 const useGetPostById = () => {
-    const [isLoading, setIsLoading] = useState(true);
+	const [isLoading, setIsLoading] = useState(true);
 	const [post, setPost] = useState(null);
-    const showToast = useShowToast();
+	const showToast = useShowToast();
 
-
-		const getPost = async (postId) => {
-			setIsLoading(true);
-			setPost(null);
-			try {
-				const postRef = await getDoc(doc(firestore, "posts", postId));
-				if (postRef.exists()) {
-					setPost(postRef.data());
-				}
-			} catch (error) {
-				showToast("Error", error.message, "error");
-				console.error(error);
-			} finally {
-				setIsLoading(false);
+	const getPost = async (postId) => {
+		setIsLoading(true);
+		setPost(null);
+		try {
+			const postRef = await getDoc(doc(firestore, "posts", postId));
+			if (postRef.exists()) {
+				const postData = postRef.data();
+				setPost({ ...postData, id: postId });
 			}
+		} catch (error) {
+			showToast("Error", error.message, "error");
+			console.error(error);
+		} finally {
 			setIsLoading(false);
-		};
+		}
+	};
 
+	return { isLoading, post, getPost };
+};
 
-  return { isLoading, post, getPost }
-  
-}
-
-export default useGetPostById
+export default useGetPostById;
