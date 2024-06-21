0d3dc973edd1036bf41ec8642902089fbd274bbf
sinox2003 <mohaennouass@gmail.com>
2024-06-21 16:21:36 +0100

    update

--- src/sharedView/chat/components/PhoneChat-UsersList.jsx
+++ src/sharedView/chat/components/PhoneChat-UsersList.jsx
@@ -56,7 +56,7 @@ function PhoneChatUsersList() {
             <HStack w={'full'} px={4} pb={1}>
                 <Text fontSize={'md'} fontWeight={'bold'}>Messages</Text>
             </HStack>
-            <VStack overflowY='scroll' h={'full'} w={'full'}>
+            <VStack overflowY='scroll' h={'full'} w={'full'} pb={3}>
                 {
                     loading ?
                         Array.from({ length: 4 }).map((_, index) => (
