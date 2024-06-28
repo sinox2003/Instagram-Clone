d68e9cd18609ebdec9260980f4f77f00f616bfc9
sinox2003 <mohaennouass@gmail.com>
2024-06-28 12:52:32 +0100

    update

--- src/sharedView/chat/components/PhoneChat-UserItem.jsx
+++ src/sharedView/chat/components/PhoneChat-UserItem.jsx
@@ -41,12 +41,12 @@ function PhoneChatUserItem({userId,myId,lastMessage,date}) {
                 <Box py={1} px={4} w={'full'} cursor={'pointer'} bg={isActive && switchMode('#262626','#EFEFEF') }
                      _hover={{ backgroundColor: switchMode('whiteAlpha.100','blackAlpha.50') }}
                 >
-                    <HStack>
+                    <HStack overflow={'hidden'}>
                         <Avatar w={'56px'} h={'56px'} src={userProfile?.profilePicURL} >
                             {/*<AvatarBadge boxSize='1em' bottom={1} right={1} borderColor={switchMode('black','white')} bg='#19D14F' />*/}
                         </Avatar>
                         <Flex w={'full'} direction={'column'} >
-                            <Text fontSize={'15px'} sx={{display: '-webkit-box', WebkitLineClamp: '1', WebkitBoxOrient: 'vertical', overflow: 'hidden'}} maxW={'full'} >
+                            <Text fontSize={'15px'} sx={{display: '-webkit-box', WebkitLineClamp: '1', WebkitBoxOrient: 'vertical', overflow: 'hidden'}} maxW={'calc(100% - 60px)'} >
                                 {userProfile?.username}
                             </Text>
                             {/*<Text fontSize={'xs'} color={'#bdbdbd'} sx={{display: '-webkit-box', WebkitLineClamp: '1', WebkitBoxOrient: 'vertical', overflow: 'hidden'}} maxW={'full'}>*/}
@@ -60,7 +60,7 @@ function PhoneChatUserItem({userId,myId,lastMessage,date}) {
                                         WebkitBoxOrient: 'vertical',
                                         overflow: 'hidden'
                                     }}
-                                    maxW={'195px'}
+                                    maxW={'calc(100% - 120px)'}
                                     mr={1} // Add margin-right for spacing between elements
                                 >
                                     {lastMessage}
